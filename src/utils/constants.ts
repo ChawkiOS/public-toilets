367e636fc479a62fcf1859ac19517dd0e5978254
ChawkiOS <chawkios2@gmail.com>
2021-11-03 18:43:23 +0100

    maps display

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,18 @@
 export const BASE_URL =
   'https://download.data.grandlyon.com/ws/grandlyon/adr_voie_lieu.adrtoilettepublique_latest/all.json';
+
+const initialPosition = { latitude: 45.764, longitude: 4.8357 };
+
+export const initialRegion = {
+  ...initialPosition,
+  latitudeDelta: 0.015,
+  longitudeDelta: 0.0121,
+};
+
+export const initialCamera = {
+  zoom: 10,
+  pitch: 0,
+  heading: 0,
+  altitude: 50000,
+  center: initialPosition,
+};
--- src/views/MapScreen.tsx
+++ src/views/MapScreen.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Marker } from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
 
-export const MapScreen = () => (
-  <View>
-    <Text>Map</Text>
-  </View>
-);
+import useToilets from '../hooks/useToilets';
+import { initialCamera, initialRegion } from '../utils/constants';
+
+export const MapScreen = () => {
+  const { toilets } = useToilets();
+
+  return (
+    <MapView
+      zoomEnabled
+      style={{ flex: 1 }}
+      initialCamera={initialCamera}
+      initialRegion={initialRegion}
+    >
+      {toilets.map((item, index) => (
+        <Marker
+          key={index}
+          tracksViewChanges={false}
+          coordinate={{
+            latitude: item.lat,
+            longitude: item.lon,
+          }}
+        />
+      ))}
+    </MapView>
+  );
+};
