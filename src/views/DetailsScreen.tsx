497cd782bd3a9a8363288b930df84a0b1b7f48d9
ChawkiOS <chawkios2@gmail.com>
2021-11-03 20:07:20 +0100

    details view params

--- src/views/DetailsScreen.tsx
+++ src/views/DetailsScreen.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { RouteProp, useRoute } from '@react-navigation/core';
 
-export const DetailsScreen = () => (
-  <View>
-    <Text>Details</Text>
-  </View>
-);
+import { ToiletType } from '../types';
+
+type DetailsParams = {
+  Params: {
+    toilet: ToiletType;
+  };
+};
+
+export const DetailsScreen = () => {
+  const route = useRoute<RouteProp<DetailsParams, 'Params'>>();
+  const { toilet } = route.params;
+
+  return (
+    <View>
+      <Text>{toilet.adresse}</Text>
+      <Text>{toilet.infoloc}</Text>
+    </View>
+  );
+};
